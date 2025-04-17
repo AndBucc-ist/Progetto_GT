c0c055c2e5e7189df115458764e3d487effe5a96
AlessandroBone16 <a.bonetti16@studenti.unibg.it>
2025-04-17 11:14:42 +0200

    Aggiunta di alcuni commenti

--- Progetto_GT/src/gioco/carte/Mazzo.java
+++ Progetto_GT/src/gioco/carte/Mazzo.java
@@ -7,7 +7,7 @@ public class Mazzo {
 	private ArrayList<CartaAvventura> mazzoCarte;
 	
 	public Mazzo() {
-		// Ci sono 3 livelli: 1 - Sono 8 carte
+		// Ci sono 3 livelli: 1 - Sono 8 carte 2 e 3 non so
 	}
 
 }
