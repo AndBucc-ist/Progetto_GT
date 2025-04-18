1cc3c3e8759a838a0541db755aba48e73e9c4a0a
AlessandroBone16 <a.bonetti16@studenti.unibg.it>
2025-04-18 22:01:25 +0200

    Aggiunta della funzione aggiungiCarteLivelloUno

--- Progetto_GT/src/gioco/carte/Mazzo.java
+++ Progetto_GT/src/gioco/carte/Mazzo.java
@@ -8,6 +8,23 @@ public class Mazzo {
 	
 	public Mazzo() {
 		// Ci sono 3 livelli: 1 - Sono 8 carte 2 e 3 non so
+		mazzoCarte = new ArrayList<>();
+		
 	}
 
+	public void aggiungiCarteLivelloUno(ArrayList<CartaAvventura> mazzo) {
+		int count = 0;
+		//Aggiungo esattamente 8 carte, indipendentemente da quante ne vengano passate così formo il mazzo da 8
+		if(mazzo.size() >= 8) {
+			for(CartaAvventura elemento: mazzo) {
+				if(count == 8) {
+					break;
+				}
+				
+				mazzoCarte.add(elemento);
+				count++;
+			}
+		}
+	}
+	
 }
