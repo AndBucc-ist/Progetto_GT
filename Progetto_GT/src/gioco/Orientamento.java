0a19403ffc4fa79dc73b7a4a77bdcbb04c3a3e8d
AlessandroBone16 <a.bonetti16@studenti.unibg.it>
2025-06-07 17:44:17 +0200

    Aggiunta la funzione statica per il casuale

--- Progetto_GT/src/gioco/Orientamento.java
+++ Progetto_GT/src/gioco/Orientamento.java
@@ -1,7 +1,20 @@
 package gioco;
 
+import java.util.Random;
+
 public enum Orientamento {
 	NORD, SUD, EST, OVEST;
 	
-	
+	public static Orientamento orientamentoCasuale() {
+		Random rand = new Random();
+		int h = rand.nextInt(4)+1;
+		
+		switch(h) {
+			case 1: return Orientamento.NORD;
+			case 2: return Orientamento.SUD;
+			case 3: return Orientamento.EST;
+			case 4: return Orientamento.OVEST;
+			default: return Orientamento.NORD;	
+		}
+	}
 }
