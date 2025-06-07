623209c925070b580881c1060d994f1385d88a29
AlessandroBone16 <a.bonetti16@studenti.unibg.it>
2025-06-07 17:44:23 +0200

    Aggiunta la funzione statica per il casuale

--- Progetto_GT/src/gioco/TipoConnettore.java
+++ Progetto_GT/src/gioco/TipoConnettore.java
@@ -1,5 +1,20 @@
 package gioco;
 
+import java.util.Random;
+
 public enum TipoConnettore {
 	SEMPLICE, DOPPIO, UNIVERSALE, NESSUNO;
+
+	public static TipoConnettore connettoreCasuale() {
+		Random rand = new Random();
+		int h = rand.nextInt(4)+1;
+		
+		switch(h) {
+			case 1: return TipoConnettore.SEMPLICE;
+			case 2: return TipoConnettore.DOPPIO;
+			case 3: return TipoConnettore.UNIVERSALE;
+			case 4: return TipoConnettore.NESSUNO;
+			default: return TipoConnettore.NESSUNO;	
+		}
+	}
 }
