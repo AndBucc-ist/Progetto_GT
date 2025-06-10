5f734d2c314e24635ae96aa21c413f2343275305
AlessandroBone16 <a.bonetti16@studenti.unibg.it>
2025-06-10 17:10:48 +0200

    Aggiunta try catch nel main

--- Progetto_GT/src/gioco/Main.java
+++ Progetto_GT/src/gioco/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-			Gioco gioco= new Gioco();
-			gioco.iniziaPartita();
+			
+			try {
+				Gioco gioco= new Gioco();
+				gioco.iniziaPartita();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 }
 	
