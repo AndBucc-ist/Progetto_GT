f18ea292fdc3f47cfd861edf3b4474551591e637
Semez23 <s.bertocchi10@studenti.unibg.it>
2025-06-12 10:17:14 +0200

    Update Main.java
    javadoc

--- Progetto_GT/src/gioco/Main.java
+++ Progetto_GT/src/gioco/Main.java
@@ -1,8 +1,19 @@
 package gioco;
-
+/**
+ * Classe principale del programma.
+ * 
+ * Serve per far partire il gioco.
+ */
 public class Main {
 
-	
+/**
+* Metodo principale. Viene eseguito per primo quando si avvia il programma.
+* 
+* Crea un nuovo gioco e lo avvia.
+* Se succede un errore, lo stampa.
+*
+* @param args argomenti da riga di comando (non usati)
+*/
 	public static void main(String[] args) {
 			
 			try {
