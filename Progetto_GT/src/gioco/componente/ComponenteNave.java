d4d7e4ab0db17a490929a2e9cdac626d469d3092
Semez23 <s.bertocchi10@studenti.unibg.it>
2025-06-12 11:01:07 +0200

    Update ComponenteNave.java
    javadoc

--- Progetto_GT/src/gioco/componente/ComponenteNave.java
+++ Progetto_GT/src/gioco/componente/ComponenteNave.java
@@ -4,9 +4,13 @@ import java.util.Random;
 
 import gioco.Orientamento;
 import gioco.TipoConnettore;
-
+ /**
+ * Rappresenta un componente della nave con un tipo, posizione, orientamento e tipo di connettore.
+ */
 public  class ComponenteNave {
-	
+ /**
+ * Enum che definisce i tipi di componenti disponibili per la nave.
+ */
 public enum TipoComponente {
 	    Batteria, CabinaCentrale, CabinaEquipaggio, Cannone, Laser, ModuloCarica,
 	    ModuloEnergia, ModuloSpeciale, Motore, Scudo
@@ -18,14 +22,23 @@ public enum TipoComponente {
 	private int posizioneY;
 	private Orientamento direzione;
 	private TipoConnettore tipoConnettore;
-	
+     /**
+     * Costruisce un componente nave con tipo e id specificati.
+     * 
+     * @param tipo tipo del componente (es. Cannone, Motore)
+     * @param id identificativo univoco del componente
+     */
 	
 	public ComponenteNave(TipoComponente tipo, int id) {
 		this.id=id;
 		this.Tipo = tipo;
 		
 	}
-	
+     /**
+     * Costruisce un componente nave con solo id (tipo da impostare successivamente).
+     * 
+     * @param id identificativo univoco del componente
+     */
 	public ComponenteNave(int id) {
 		this.id=id;
 	} 
